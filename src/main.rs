41ac17f4b3d5664969c51e2f6066a3ceb506586f
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2024-01-26 12:13:51 -0800

    bootloader & print output

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello Rust OS!";
 
 #[no_mangle] //No name mangling for this function
 pub extern "C" fn _start() -> ! {
     // this func defines the entry point
+    let vga_buffer = 0xb800 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
