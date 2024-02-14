aed5ced2f5ecb5e9c863e7395796d1dab345f168
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2024-02-13 19:24:26 -0800

    fixed missing zero, works now

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ static HELLO: &[u8] = b"Hello Rust OS!";
 #[no_mangle] //No name mangling for this function
 pub extern "C" fn _start() -> ! {
     // this func defines the entry point
-    let vga_buffer = 0xb800 as *mut u8;
+    let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
