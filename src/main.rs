74eaf2e15cb06579752b52f6c51c44f2e8dbcede
Amber Laptop <amberadelaideshore@gmail.com>
2024-02-20 15:31:31 -0800

    before serial port

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@
 #![no_std]
 #![no_main]
 
+// testing
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 
 mod vga_buffer;
@@ -16,7 +21,10 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     // this func defines the entry point
     println!("Hello Rust{}", "!");
-    panic!("The Disco!");
+    //panic!("The Disco!");
+    #[cfg(test)]
+    test_main();
+
 
     loop {}
 }
@@ -26,4 +34,39 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info); //why does the msg follow the location? change?
     loop {}
+}
+
+//testing
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
+// exiting QEMU
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
