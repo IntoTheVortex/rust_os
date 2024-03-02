950a80267536f4972bbaffab5b6e50e523a9e27a
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2024-03-01 17:46:43 -0800

    fixed serial output, timeout demonstration

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@
 
 
 mod vga_buffer;
+mod serial; 
 
 use core::panic::PanicInfo;
 
@@ -31,24 +32,55 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// This function is called on panic:
+#[cfg(not(test))] //run this if not test run
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info); //why does the msg follow the location? change?
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 //testing
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion! ");
+    serial_print!("trivial assertion! ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
+    loop {}
+}
+
+
+// Exit Qemu:
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
