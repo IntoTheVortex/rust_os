0f8f4722be840be8297d932bb8c7cb1496167028
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2024-03-04 09:13:18 -0800

    insert print automatically for tests, done

--- src/main.rs
+++ src/main.rs
@@ -51,20 +51,35 @@ fn panic(info: &PanicInfo) -> ! {
 
 //testing
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion! ");
+    //serial_print!("trivial assertion! ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
-    loop {}
+    //serial_println!("[ok]");
+}
+
+// make test_runner print automatically
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
 
 
