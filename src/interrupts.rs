79949e89900979416d5c146c4381d35d52f44302
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2024-03-06 17:48:28 -0800

    init double fault section

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -23,3 +23,12 @@ extern "x86-interrupt" fn breakpoint_handler(
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
+
+
+//Tests
+
+#[test_case]
+fn test_breakpoint_exception() {
+    //invoke breakpoint exception:
+    x86_64::instructions::interrupts::int3();
+}
